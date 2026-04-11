d0a8909155906c0c1660dcc20af431f602fe3abb
Fareed86 <fa043541@gmail.com>
2026-04-11 21:44:32 +0600

    first commit

---
+++ cypress/e2e/login/login.spec.cy.js
@@ -0,0 +1,9 @@
+describe('login',()=>{
+    it('should login with valid credentials', ()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('[name="username"]').type('Admin');
+        cy.get('[name="password"]').type('admin123');
+        cy.get('.orangehrm-login-button').click();
+        cy.url().should('include','dashboard');
+    })
+})
